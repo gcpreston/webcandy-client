a8cf4809548d0ebf591726db6a4dcf57cb6b8ad7
Graham Preston <preston.gr@husky.neu.edu>
2019-09-03 23:00:00 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme:
 
 setup(
     name='webcandy-client',
-    version='0.1.0',
+    version='0.1.1',
     author='Graham Preston',
     author_email='graham.preston@gmail.com',
     description='Client-side code for communicating with a Webcandy server.',
