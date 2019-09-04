e73d71de541407f0487fef206fce1a360d5c76c7
Graham Preston <preston.gr@husky.neu.edu>
2019-09-04 17:14:16 -0400

    Include LICENSE in distribution

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme:
 
 setup(
     name='webcandy-client',
-    version='0.1.1',
+    version='0.1.2',
     author='Graham Preston',
     author_email='graham.preston@gmail.com',
     description='Client-side code for communicating with a Webcandy server.',
@@ -13,6 +13,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/gcpreston/webcandy-client',
     packages=find_packages(),
+    include_package_data=True,
     zip_safe=False,
     install_requires=[
         'opclib',
@@ -24,5 +25,15 @@ setup(
             'wc-client = webcandy_client.client:main',
             'wc-controller = webcandy_client.controller:main'
         ]
+    },
+    # python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    project_urls={
+        'Source': 'https://github.com/gcpreston/webcandy-client',
     }
 )
